15d9db7024f5dd7372d2c0efb512f871875d382b
grietbeyens <beyensgriet@gmail.com>
2022-11-30 12:56:09 +0100

    feat: add navbar with styling

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,11 @@
 <script setup>
-import { ref, computed } from 'vue'
-import Home from './components/Home.vue';
-import Gallery from './components/Gallery.vue';
-import Configurator from './components/Configurator.vue'
-import NotFound from './components/NotFound.vue';
-
-const routes = {
-  '/': Home,
-  '/gallery': Gallery,
-  '/configurator': Configurator,
-}
-
-const currentPath = ref(window.location.hash)
-
-window.addEventListener('hashchange', () => {
-  currentPath.value = window.location.hash
-})
-
-const currentView = computed(() => {
-  return routes[currentPath.value.slice(1) || '/'] || NotFound
-})
+//components
+import Navbar from './components/Navbar.vue';
 </script>
 
 <template>
   <div>
-    <a href="#/">Home</a> |
-    <a href="#/configurator">Configurator</a> |
-    <a href="#/gallery">Gallery</a> |
-    <a href="#/broken-link">Broken Link</a>
-    <component :is="currentView" />
+    <Navbar />
   </div>
 </template>
 
