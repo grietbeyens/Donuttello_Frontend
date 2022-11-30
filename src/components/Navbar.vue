dad568f1d3c11db7c331cd94d8c5bfdf877ab4ef
grietbeyens <beyensgriet@gmail.com>
2022-11-30 12:55:28 +0100

    refactor: restructure components and pages

---
+++ src/components/Navbar.vue
@@ -0,0 +1,72 @@
+<script setup>
+import { ref, computed } from 'vue'
+import Home from '../pages/Home.vue';
+import Gallery from '../pages/Gallery.vue';
+import Configurator from '../pages/Configurator.vue'
+import NotFound from '../pages/NotFound.vue';
+
+const routes = {
+    '/': Home,
+    '/gallery': Gallery,
+    '/configurator': Configurator,
+}
+
+const currentPath = ref(window.location.hash)
+
+window.addEventListener('hashchange', () => {
+    currentPath.value = window.location.hash
+})
+
+const currentView = computed(() => {
+    return routes[currentPath.value.slice(1) || '/'] || NotFound
+})
+</script>
+
+<template>
+    <nav class="navbar">
+        <div class="navbar__logo">
+            <a href="#/"><img src="../assets/donuttello-logo.png" alt="logo"></a>
+        </div>
+        <div class="navbar__links">
+            <a href="#/">Home</a> 
+            <a href="#/configurator">Configurator</a> 
+            <a href="#/gallery">Gallery</a> 
+        </div>
+    </nav>
+    <component :is="currentView" />
+</template>
+
+<style scoped>
+.navbar{
+    background-color: var(--primary-pink);
+    color: var(--neutral);
+    width: 100%;
+    padding: 1.2em;
+    position: fixed;
+    top: 0;
+}
+
+a {
+    color: var(--neutral);
+    text-decoration: none;
+    margin-right: 2em;
+    text-transform: uppercase;
+}
+
+.navbar__links{
+    display: flex;
+    justify-content: flex-end;
+    font-family: var(--title);
+    padding-right: 1em;
+}
+
+.navbar__logo{
+    position: absolute;
+    left: calc(10vw)
+}
+
+.navbar__logo img{
+    width: 6em;
+}
+</style>
+
