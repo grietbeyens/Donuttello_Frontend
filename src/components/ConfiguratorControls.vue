4d275e09306031a6151fa52b79d6ca01d3ffb5a5
grietbeyens <beyensgriet@gmail.com>
2022-12-02 17:40:10 +0100

    feat: add homepage

--- src/components/ConfiguratorControls.vue
+++ src/components/ConfiguratorControls.vue
@@ -25,31 +25,31 @@ const currentView = computed(() => {
         <div class="controls">
             <div class="padding-left">
             <div class="margin">
-                <h2 class="title">Algemeen</h2>
+                <h2 class="title title--secondary">Algemeen</h2>
                 <div class="margin__small">
-                    <label class="subtitle" for="bedrijfsnaam">Bedrijfsnaam <span class="obligated">*</span>:</label>
+                    <label class="title title--tertiary " for="bedrijfsnaam">Bedrijfsnaam <span class="obligated">*</span>:</label>
                     <input class="input__text text" type="text" id="bedrijfsnaam" name="bedrijfsnaam"
                         v-model="bedrijfsnaam" placeholder="Donuttello">
                 </div>
                 <div class="margin__small">
-                    <label class="subtitle" for="email">Mijn email adres <span class="obligated">*</span>:</label>
+                    <label class="title title--tertiary " for="email">Mijn email adres <span class="obligated">*</span>:</label>
                     <input class="input__text text" type="text" id="email" name="email" v-model="email"
                         placeholder="bedrijf@email.com">
                 </div>
             </div>
             <div class="margin">
-                <h2 class="title">Topping</h2>
+                <h2 class="title title--secondary">Topping</h2>
                 <div class="margin__small">
-                    <label class="subtitle" for="glazuur">Glazuur <span class="obligated">*</span>:</label>
+                    <label class="title title--tertiary " for="glazuur">Glazuur <span class="obligated">*</span>:</label>
                     <input class="input__color" type="color" id="glazuur" name="glazuur" v-model="glazuur">
                 </div>
                 <div class="margin__small">
-                    <label class="subtitle" for="topping">Topping <span class="obligated">*</span>:</label>
+                    <label class="title title--tertiary " for="topping">Topping <span class="obligated">*</span>:</label>
                     <input class="input__color" type="color" id="topping" name="topping" v-model="glazuur">
                 </div>
             </div>
             <div class="margin">
-                <h2 class="title">Logo</h2>
+                <h2 class="title title--secondary">Logo</h2>
                 <p class="text margin__small">Onze bakkers kunnen jouw logo op een stukje chocolade drukken en plaatsen
                     deze dan op elke donut.</p>
                 <div class="margin">
@@ -76,19 +76,19 @@ const currentView = computed(() => {
                 </div>
             </div>
             <div class="margin">
-                <h2 class="title">Bijna klaar</h2>
+                <h2 class="title title--secondary">Bijna klaar</h2>
                 <div class="margin__small">
-                    <label class="subtitle" for="donut">Zo heet mijn donut <span class="obligated">*</span>:</label>
+                    <label class="title title--tertiary " for="donut">Zo heet mijn donut <span class="obligated">*</span>:</label>
                     <input class="input__text text" type="text" id="donut" name="donut" v-model="donut"
                         placeholder="Sprikelicious">
                 </div>
                 <div class="margin__small">
-                    <label class="subtitle" for="aantal">Aantal <span class="obligated">*</span>:</label>
+                    <label class="title title--tertiary " for="aantal">Aantal <span class="obligated">*</span>:</label>
                     <input class="input__text text" type="text" id="aantal" name="aantal" v-model="donut"
                         placeholder="100">
                 </div>
                 <div class="margin__small">
-                    <label class="subtitle" for="extra">Dit wil ik nog zeggen:</label>
+                    <label class="title title--tertiary " for="extra">Dit wil ik nog zeggen:</label>
                     <textarea class="input__text text" name="extra" id="extra" rows="10" resize="none"
                         placeholder="Ik wil 1 donut zonder sprinkels."></textarea>
                 </div>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -2,12 +2,78 @@
 </script>
 
 <template>
-    <div class="nav-margin">
-        <h2>Home</h2>
+    <div class="nav-margin padding-top ">
+        <div class="flex flex--center flex--col">
+            <h1 class="title margin">Jouw <span class="obligated">eigen</span> donut creëren?</h1>
+            <a class="obligated btn btn--yellow" href="#">Dat kan hier!</a>
+        </div>
+        <div>
+            <img class="pattern" src="../assets/patroon.png" alt="patroon">
+        </div>
+        <div class="text-container">
+            <h2 class="title title--secondary"><span class="obligated">WOW</span>, hoe werkt dat?</h2>
+            <p class="text margin__small">Gewoon simpelweg vanuit jouw zetel!
+            Via onze speciale configurator heb je de
+            mogelijkheid om helemaal online je eigen donut
+            te creëren.
+            </p>
+            <p class="margin__small">
+            Onze bakkerijen kunnen de chocoladeglazuur
+            elke kleur geven die jij wil! Daarnaast kan je
+            ook nog een eetbaar logo op je donut laten
+            plaatsen. Perfect voor een verassende traktatie
+            in jouw bedrijf!
+            </p>
+        </div> 
+        <div>
+            <img class="mascotte" src="../assets/mascotte.png" alt="mascotte">
+        </div>  
+        <footer class="flex flex--center flex--col">
+            <p class="title title--tertiary">Is jouw bedrijf op zoek naar een verrassende traktatie?</p>
+            <a class="btn margin__small" href="#">Klik dan hier!</a>
+        </footer>
     </div>
 </template>
 
 <style scoped>
+h1{
+    width: 300px;
+    text-align: center;
+}
 
+.padding-top{
+    padding-top: 8em;
+}
 
+.text-container{
+    margin: 2em;
+}
+
+.pattern{
+    background-size: cover;
+    width: 100%;
+    margin: 7em 0 1em 0;
+}
+
+.title--secondary{
+    color: var(--dark);
+}
+
+.title--tertiary{
+    width: 12em;
+}
+
+.mascotte{
+    width: 12em;
+    position: absolute;
+    right: 3em;
+    z-index: -1;
+}
+
+footer{
+    background-color: var(--primary-blue);
+    padding: 2em;
+    text-align: center;
+    margin-top: 16em;
+}
 </style>
