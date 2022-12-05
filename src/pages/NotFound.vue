2e51261c86f8547621b83ee8911e2295822223c2
grietbeyens <beyensgriet@gmail.com>
2022-12-05 21:59:55 +0100

    feat: add 404 page

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -2,11 +2,26 @@
 </script>
 
 <template>
-    <div>
-        <h2>niet gevonden</h2>
+    <div class="flex flex--center">
+        <div class="nav-margin">
+        <h2 class="title"><span class="obligated">Oeps!</span> Het lijkt erop dat alle donuts en inhoud van deze pagina opgegeten zijn...</h2>
+        <img class="mascotte" src="../assets/mascotte-404.png" alt="mascotte">
+        </div>
     </div>
 </template>
 
 <style scoped>
-
+.nav-margin{
+    margin: 7em 2em 0 2em;
+    max-width: 33em;
+}
+.title{
+    font-size: 2em;
+}
+.mascotte{
+    position: absolute;
+    width: 14em;
+    bottom: 3em;
+    margin: auto auto;
+}
 </style>
