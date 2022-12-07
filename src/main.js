1a62fabac7176ebd3ed4a941012f9642b402cb93
grietbeyens <beyensgriet@gmail.com>
2022-12-07 16:11:55 +0100

    feat: add plate

--- src/main.js
+++ src/main.js
@@ -4,11 +4,16 @@ import App from "./App.vue";
 
 createApp(App).mount("#app");
 
-document.getElementById("btn-status").addEventListener("click", () => {
-    console.log("clicked");
-    document.getElementById("btn-status").classList.toggle("btn-blue");
-    if ((document.getElementById("btn-status").classList.contains("btn-blue")))
-      document.getElementById("btn-status").innerHTML = "Klaar";
-    else document.getElementById("btn-status").innerHTML = "In afwachting";
-  
-});
+// document.getElementById("btn-status").addEventListener("click", () => {
+//     console.log("clicked");
+//     document.getElementById("btn-status").classList.toggle("btn-blue");
+//     if ((document.getElementById("btn-status").classList.contains("btn-blue")))
+//       document.getElementById("btn-status").innerHTML = "Klaar";
+//     else document.getElementById("btn-status").innerHTML = "In afwachting";
+
+// });
+
+
+
+
+
