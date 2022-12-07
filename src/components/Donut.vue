05cc64f1c8e32226bb3475892380b1a6a969e97f
grietbeyens <beyensgriet@gmail.com>
2022-12-07 18:24:48 +0100

    feat: display cards on screen

--- src/components/Donut.vue
+++ src/components/Donut.vue
@@ -37,13 +37,13 @@ onMounted(() => {
     const rechthoekGeometry = new THREE.BoxGeometry(2, 1, 1);
     const rechthoekMaterial = new THREE.MeshBasicMaterial({ color: 0xffffff, side: THREE.FrontSide });
     const rechthoek = new THREE.Mesh(rechthoekGeometry, rechthoekMaterial);
-    rechthoek.position.x = 0;
-    rechthoek.position.y = 1.7;
-    rechthoek.position.z = -1;
-    rechthoek.scale.x = 1;
-    rechthoek.scale.y = 0.1;
-    rechthoek.scale.z = 1;
-    rechthoek.rotation.x = 0.5;
+    rechthoek.position.x = -3; //0
+    rechthoek.position.y = 5; //2.4
+    rechthoek.position.z = -1;  //-1
+    rechthoek.scale.x = 1; //1
+    rechthoek.scale.y = 0.1; //0.1
+    rechthoek.scale.z = 1; //1
+    rechthoek.rotation.x = 1.5; //1.5
 
     const checkboxRechthoek = document.querySelector("#rechthoek");
     checkboxRechthoek.addEventListener("change", () => {
@@ -56,6 +56,81 @@ onMounted(() => {
             scene.remove(rechthoek);
         }
     });
+
+    //kaartje vierkant
+    const vierkantGeometry = new THREE.BoxGeometry(1, 1, 1);
+    const vierkantMaterial = new THREE.MeshBasicMaterial({ color: 0xffffff });
+    const vierkant = new THREE.Mesh(vierkantGeometry, vierkantMaterial);
+    vierkant.position.x = -3.4;
+    vierkant.position.y = 5;
+    vierkant.position.z = -1;
+    vierkant.scale.x = 1;
+    vierkant.scale.y = 0.1;
+    vierkant.scale.z = 1;
+    vierkant.rotation.x = 1.5;
+
+    const checkboxVierkant = document.querySelector("#vierkant");
+    checkboxVierkant.addEventListener("change", () => {
+        if (checkboxVierkant.checked) {
+            scene.remove(rechthoek);
+            scene.remove(cirkel);
+            scene.remove(ovaal);
+            scene.add(vierkant);
+        } else {
+            scene.remove(vierkant);
+        }
+    });
+
+
+    //kaartje cirkel
+    const cirkelGeometry = new THREE.CylinderGeometry(0.6, 0.6, 1, 32);
+    const cirkelMaterial = new THREE.MeshBasicMaterial({ color: 0xffffff });
+    const cirkel = new THREE.Mesh(cirkelGeometry, cirkelMaterial);
+    cirkel.position.x = -3.4;
+    cirkel.position.y = 5;
+    cirkel.position.z = -1;
+    cirkel.scale.x = 1;
+    cirkel.scale.y = 0.1;
+    cirkel.scale.z = 1;
+    cirkel.rotation.x = 1.5;
+
+    const checkboxCirkel = document.querySelector("#cirkel");
+    checkboxCirkel.addEventListener("change", () => {
+        if (checkboxCirkel.checked) {
+            scene.remove(rechthoek);
+            scene.remove(vierkant);
+            scene.remove(ovaal);
+            scene.add(cirkel);
+        } else {
+            scene.remove(cirkel);
+        }
+    });
+
+
+    //kaartje ovaal
+    const ovaalGeometry = new THREE.CylinderGeometry(0.6, 0.6, 1, 32);
+    const ovaalMaterial = new THREE.MeshBasicMaterial({ color: 0xffffff });
+    const ovaal = new THREE.Mesh(ovaalGeometry, ovaalMaterial);
+    ovaal.position.x = -2.9;
+    ovaal.position.y = 5;
+    ovaal.position.z = -1;
+    ovaal.scale.x = 2;
+    ovaal.scale.y = 0.1;
+    ovaal.scale.z = 1;
+    ovaal.rotation.x = 1.5;
+
+    const checkboxOvaal = document.querySelector("#ovaal");
+    checkboxOvaal.addEventListener("change", () => {
+        if (checkboxOvaal.checked) {
+            scene.remove(rechthoek);
+            scene.remove(vierkant);
+            scene.remove(cirkel);
+            scene.add(ovaal);
+        } else {
+            scene.remove(ovaal);
+        }
+    });
+
 });
 
 window.addEventListener("resize", onWindowResize, false);
