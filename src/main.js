25fb1644ef586d2fbafc4bf3ecf11ed473b319c0
grietbeyens <beyensgriet@gmail.com>
2022-12-12 14:48:46 +0100

    refactor: cleanup main.js

--- src/main.js
+++ src/main.js
@@ -4,16 +4,6 @@ import App from "./App.vue";
 
 createApp(App).mount("#app");
 
-// document.getElementById("btn-status").addEventListener("click", () => {
-//     console.log("clicked");
-//     document.getElementById("btn-status").classList.toggle("btn-blue");
-//     if ((document.getElementById("btn-status").classList.contains("btn-blue")))
-//       document.getElementById("btn-status").innerHTML = "Klaar";
-//     else document.getElementById("btn-status").innerHTML = "In afwachting";
-
-// });
-
-
 
 
 
